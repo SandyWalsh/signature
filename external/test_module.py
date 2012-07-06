873ca2d0772be5fb176b2a43fa51dd9939a9baca
SandyWalsh <sandy.walsh@rackspace.com>
2012-07-06 15:21:12 -0300

    file loading and external module checks

---
+++ external/test_module.py
@@ -0,0 +1,11 @@
+class Foo(object):
+    def method_a(self, a, b, c, d):
+        pass
+
+
+class Blah(object):
+    def method_a(self, a, b, c, d):
+        pass
+
+    def method_b(self, a, b, c, e):
+        pass
