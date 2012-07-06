c0f899861a76c750b84f07ea37e201016a80b8a3
SandyWalsh <sandy.walsh@rackspace.com>
2012-07-06 16:19:41 -0300

    raise on error

--- README.md
+++ README.md
@@ -6,3 +6,25 @@ Signature is a simple utility for doing signature checking of Python code.
 This is useful for comparing method/function signatures of mocked unit test code 
 against production code. If the signatures no longer match up, you can catch
 these errors early without having to resort to complicated integration tests.
+
+For example:
+
+Let's say we had a class that had a method with a specific signature:
+
+class Foo(object):
+    def replace_me(self, a, b=1, c="hello"):
+        pass
+
+and another object that could take the place of Foo (like a "driver" or "plugin"
+mechanism):
+
+class Blah(object):
+    def real_implementation(self, a, b=1, c="hello"):
+        pass
+
+Everything is fine so long as the signatures for replace_me and real_implementation
+are the same. Unit tests generally won't find these sorts of bugs, but Signature can.
+
+import signature
+check(dict(mates=[['__main__:Foo.replace_me', '__main__:Blah.real_implementation']]),
+      raise_on_error=True)
--- watch.py
+++ watch.py
@@ -35,6 +35,45 @@ def find_code(target):
     return getattr(klass_object, function)
 
 
+class DifferentSignatures(Exception):
+    def __init__(self, this_code, this_sig, that_code, that_sig):
+        super(DifferentSignatures, self).__init__()
+        self.this_code = this_code
+        self.this_sig = this_sig
+        self.that_code = that_code
+        self.that_sig = that_sig
+
+    def __str__(self):
+        return """
+%s %s
+%s %s""" % (self.this_code, self.this_sig, self.that_code, self.that_sig)
+
+
+def check(config, raise_on_error=False):
+    mates = config['mates']
+    results = []
+    for block in mates:
+        last_mate = None
+        last_sig = None
+        sigs = []
+        mismatch = False
+        for mate in block:
+            code = find_code(mate)
+            sig = inspect.getargspec(code)
+            if last_sig and sig != last_sig:
+                mismatch = True
+                if raise_on_error:
+                    raise DifferentSignatures(mate, sig, last_mate, last_sig)
+            last_mate = mate
+            last_sig = sig
+            sigs.append((mate, sig))
+        results.append((mismatch, sigs))
+    return results
+
+
+#--------------------------
+# Sample functions/methods for testing
+
 class Foo(object):
     def method_a(self, a, b, c, d):
         pass
@@ -59,32 +98,16 @@ def function_b(a, b, c, d):
 def function_c(a, b, c, e):
     pass
 
-
-def check(config):
-    mates = config['mates']
-
-    results = []
-    for block in mates:
-        last_mate = None
-        last_sig = None
-        sigs = []
-        mismatch = False
-        for mate in block:
-            code = find_code(mate)
-            sig = inspect.getargspec(code)
-            if last_sig and sig != last_sig:
-                mismatch = True
-            last_mate = mate
-            last_sig = sig
-            sigs.append((mate, sig))
-        results.append((mismatch, sigs))
-    return results
+#--------------------------
 
 
 if __name__ == '__main__':
-    with open('sample.json') as f:
+    filename = 'sample.json'
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    with open(filename) as f:
         config = json.load(f)
-    results = check(config)
+    results = check(config)  #, raise_on_error=True)
 
     for mismatch, sigs in results:
         if mismatch:
